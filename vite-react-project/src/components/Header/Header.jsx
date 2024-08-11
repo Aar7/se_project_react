f58b62331db998394c3fc8cba724bc4ad9b99e6b
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-08-11 07:39:10 -0500

    (feat) Create basic header. Create blank fonts file

---
+++ vite-react-project/src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+// import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__wrapper header__logo-date-location-wrapper">
+        <p className="header__logo">wtwr°</p>
+        <p className="header__date-location">DATE-LOCATION</p>
+      </div>
+      <div className="header__wrapper header__button-profile-wrapper">
+        <button className="header__add-clothes">+ Add clothes</button>
+        <div className="header__profile">
+          <p className="header__profile-name">NAME</p>
+          <img className="header__profile-image"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
