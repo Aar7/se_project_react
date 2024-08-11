6ad546374dd1793af989e108e57821513424fac2
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-08-11 08:57:30 -0500

    (feat) Code Header to mirror design. Add various assets to project

--- vite-react-project/src/components/Header/Header.jsx
+++ vite-react-project/src/components/Header/Header.jsx
@@ -1,17 +1,22 @@
 import profImage from "../../assets/images/profile-image-placeholder.svg";
+import logo from "../../assets/images/wtwr-logo.svg";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
       <div className="header__wrapper header__logo-date-location-wrapper">
-        <p className="header__logo">wtwr°</p>
-        <p className="header__date-location">DATE-LOCATION</p>
+        <img className="header__logo" src={logo} />
+        <p className="header__date-location">Date, Location</p>
       </div>
+
       <div className="header__wrapper header__button-profile-wrapper">
-        <button className="header__add-clothes">+ Add clothes</button>
+        <button className="header__add-clothes" type="button">
+          + Add clothes
+        </button>
+
         <div className="header__profile">
-          <p className="header__profile-name">NAME</p>
+          <p className="header__profile-name">Name, Namerson</p>
           <img className="header__profile-image" src={profImage} />
         </div>
       </div>
