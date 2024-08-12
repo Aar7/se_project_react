b0d1df1dec7f4854a89bb09917e9312af997549e
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-08-12 14:58:17 -0500

    (feat, fix) Add default array and create weatherApi.js. Fix structure of Main->ItemCard

--- vite-react-project/src/components/Main/ItemCard/ItemCard.jsx
+++ vite-react-project/src/components/Main/ItemCard/ItemCard.jsx
@@ -1,36 +1,12 @@
 import "./ItemCard.css";
 import sampleImage from "../../../assets/images/cap.svg";
 
-function ItemCard() {
+function ItemCard({ cardTitle, cardImageLink }) {
   return (
-    <div className="main__cards">
-      <p className="main__cards_suggestion">
-        Today is insanely buggy. Wear DevTools
-      </p>
-      <ul className="main__cards_cardsList">
-        {/* Sample Card */}
-        <li className="main__cards_listItem">
-          <p className="main__cards_listItem-title">One sample title</p>
-          <img className="main__cards_listItem-image" src={sampleImage} />
-        </li>
-        <li className="main__cards_listItem">
-          <p className="main__cards_listItem-title">This is a title too</p>
-          <img className="main__cards_listItem-image" src={sampleImage} />
-        </li>
-        <li className="main__cards_listItem">
-          <p className="main__cards_listItem-title">3</p>
-          <img className="main__cards_listItem-image" src={sampleImage} />
-        </li>
-        <li className="main__cards_listItem">
-          <p className="main__cards_listItem-title">A sample title for you</p>
-          <img className="main__cards_listItem-image" src={sampleImage} />
-        </li>
-        <li className="main__cards_listItem">
-          <p className="main__cards_listItem-title">Sample title 5</p>
-          <img className="main__cards_listItem-image" src={sampleImage} />
-        </li>
-      </ul>
-    </div>
+    <li className="main__cards_listItem">
+      <p className="main__cards_listItem-title">{cardTitle}</p>
+      <img className="main__cards_listItem-image" src={cardImageLink} />
+    </li>
   );
 }
 
--- vite-react-project/src/components/Main/Main.jsx
+++ vite-react-project/src/components/Main/Main.jsx
@@ -1,12 +1,39 @@
 import "./Main.css";
 import WeatherCard from "./WeatherCard/WeatherCard";
 import ItemCard from "./ItemCard/ItemCard";
+import defaultContent from "../../utils/defaultContent.js";
 function Main() {
   return (
     <>
       <div className="main">
         <WeatherCard />
-        <ItemCard />
+        <div className="main__cards">
+          <p className="main__cards_suggestion">
+            Today is insanely buggy. Wear DevTools
+          </p>
+          <ul className="main__cards_cardsList">
+            <ItemCard
+              cardTitle={defaultContent[0].name}
+              cardImageLink={defaultContent[0].link}
+            />
+            <ItemCard
+              cardTitle={defaultContent[1].name}
+              cardImageLink={defaultContent[1].link}
+            />
+            <ItemCard
+              cardTitle={defaultContent[2].name}
+              cardImageLink={defaultContent[2].link}
+            />
+            <ItemCard
+              cardTitle={defaultContent[3].name}
+              cardImageLink={defaultContent[3].link}
+            />
+            <ItemCard
+              cardTitle={defaultContent[4].name}
+              cardImageLink={defaultContent[4].link}
+            />
+          </ul>
+        </div>
       </div>
     </>
   );
