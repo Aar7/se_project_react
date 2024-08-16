0f70fb87f86ab7d54182d8c1a4c75671a0d2d687
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-08-15 21:48:23 -0500

    (fix) Fix event bubbling on modal forms

--- vite-react-project/src/components/ModalWithForm/ModalWithForm.jsx
+++ vite-react-project/src/components/ModalWithForm/ModalWithForm.jsx
@@ -5,11 +5,16 @@ function ModalWithForm(props) {
     // <div className="modal__wrapper" onClick={props.onClose}>
     <div
       className={props.formModalClasslist}
-      onClick={() => {
+      onClick={(event) => {
         props.onClose(props.setFormModalClasslist);
       }}
     >
-      <div className={`modal modal_type_${props.formName}`}>
+      <div
+        onClick={(event) => {
+          event.stopPropagation();
+        }}
+        className={`modal modal_type_${props.formName}`}
+      >
         <h2 className="modal__form-title">{props.formTitle}</h2>
         <button className="button modal__close-button"></button>
         <form className="modal__form" name={`${props.formName}`}>
