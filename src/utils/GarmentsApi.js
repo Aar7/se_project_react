392b7cf1f61bd49f703d802b21e581f8c5a06ff7
AaronMeighoo <aaronmeighoo1@gmail.com>
2024-09-02 17:18:54 -0500

    (improvement) Create _request method to neaten GarmentsApi

--- src/utils/GarmentsApi.js
+++ src/utils/GarmentsApi.js
@@ -13,30 +13,31 @@ export default class GarmentsApi {
     }
   }
 
+  async _request(url, options) {
+    return fetch(url, options).then(this._checkResponse);
+  }
+
   async getGarmentData() {
-    return fetch(`${this.baseUrl}items`).then(this._checkResponse);
+    console.log(`[GarmentsApi] garmentsApi.getGarmentData called!`);
+    return this._request(`${this.baseUrl}items`, {});
   }
 
   async saveGarmentData(cardData) {
-    // function to save garment new garments to a database
-    console.log(`[GarmentsApi] weather.saveGarmentData called!`);
-    return fetch(`${this.baseUrl}items`, {
+    console.log(`[GarmentsApi] garmentsApi.saveGarmentData called!`);
+
+    return this._request(`${this.baseUrl}items`, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(cardData),
-    }).then(this._checkResponse);
+    });
   }
 
   async deleteGarmentData(garmentId) {
-    // function to remove the desired garment from the database
-    console.log(`[GarmentsApi] weather.deleteGarment called!`);
-    return fetch(`${this.baseUrl}items/${garmentId}`, {
+    console.log(`[GarmentsApi] garmentsApi.deleteGarmentData called!`);
+
+    return this._request(`${this.baseUrl}items/${garmentId}`, {
       method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then(this._checkResponse);
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
